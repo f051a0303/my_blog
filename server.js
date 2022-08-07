62f9a1b68c3689e030c088035fb605871fa19b77
ChiMing <alexlai7777@gmail.com>
2022-08-07 15:08:22 -0500

    commit1234

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-/*********************************************************************************
-*  WEB322 – Assignment 06
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
+/****************************************************************************** 
 *  Name: _____Chi Ming Lai______ Student ID: ___158400200_____ Date: __26-07-2022_______
 *
 *  Online (Heroku) Link: ______https://boiling-earth-40990.herokuapp.com/______
